732200ce333ca1f065122b0b64cc0fbee3e9187d
prasanna <prasanna15j@gmail.com>
2024-05-23 10:36:35 +0530

    dom intro

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let heading = document.getElementById("heading");
+
+let head_class = document.getElementsByClassName("head_class");
+
+let ptag = document.getElementsByTagName("p");
+
+let data = document.querySelector(".head_class"); // first matching element
+
+let data1 = document.querySelector("p");
+
+let ans = document.querySelectorAll(".head_class");
+
+
+heading.innerText = "Hello";
+
+
+
+// console.log(ans);
+// console.dir(ans);
+
+// console.log(head_class);
+// console.log(head_class);
+
